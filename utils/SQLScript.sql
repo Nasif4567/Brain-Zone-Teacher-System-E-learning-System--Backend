bc7a69ac00136fed6e3bfe9a3d7c56fac3162d12
arbaazmir-1 <arbaazmir@icloud.com>
2024-02-28 12:29:57 +0400

    sql script updated with new database name and tables.

--- utils/SQLScript.sql
+++ utils/SQLScript.sql
@@ -1,11 +1,9 @@
--- db name: llm-portal
-
-CREATE DATABASE IF NOT EXISTS llm-portal 
+CREATE DATABASE IF NOT EXISTS `llm-portal` 
     CHARACTER SET utf8mb4
     COLLATE utf8mb4_unicode_ci;
 
--- TABLE `llm-portal`.`users` 
--- userID, username, password, email, role, created_at, updated_at
+USE `llm-portal`;
+
 CREATE TABLE IF NOT EXISTS users (
     userID VARCHAR(255) NOT NULL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
@@ -14,11 +12,8 @@ CREATE TABLE IF NOT EXISTS users (
     role VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    
-)
+);
 
--- Table `llm-portal`.`coursesCreated`
--- courseID, courseName, courseDescription, courseInstructor, created_at, updated_at, userID, courseStatus , courseImage
 CREATE TABLE IF NOT EXISTS coursesCreated (
     courseID VARCHAR(255) NOT NULL PRIMARY KEY,
     courseName VARCHAR(255) NOT NULL,
@@ -26,13 +21,12 @@ CREATE TABLE IF NOT EXISTS coursesCreated (
     courseInstructor VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (userID) REFERENCES users(userID),
+    userID VARCHAR(255) NOT NULL,
     courseStatus VARCHAR(255) NOT NULL,
-    courseImage VARCHAR(255) NOT NULL
-)
+    courseImage VARCHAR(255) NOT NULL,
+    FOREIGN KEY (userID) REFERENCES users(userID)
+);
 
--- Table `llm-portal`.`courseContent`
--- contentID, courseID, contentTitle, contentDescription, contentURL, created_at, updated_at
 CREATE TABLE IF NOT EXISTS courseContent (
     contentID VARCHAR(255) NOT NULL PRIMARY KEY,
     courseID VARCHAR(255) NOT NULL,
@@ -42,17 +36,14 @@ CREATE TABLE IF NOT EXISTS courseContent (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (courseID) REFERENCES coursesCreated(courseID)
-)
+);
 
--- Table `llm-portal`.`courseEnrollment`
--- enrollmentID, courseID, userID, created_at, updated_at
 CREATE TABLE IF NOT EXISTS courseEnrollment (
     enrollmentID VARCHAR(255) NOT NULL PRIMARY KEY,
     courseID VARCHAR(255) NOT NULL,
+    userID VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (courseID) REFERENCES coursesCreated(courseID),
     FOREIGN KEY (userID) REFERENCES users(userID)
-)
-
--- more tables to be added as needed
+);
