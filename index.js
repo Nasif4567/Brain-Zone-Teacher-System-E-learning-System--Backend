0cbc1363d6d6c67c9acafc9fbaf3e066ebfdcc1f
arbaazmir-1 <arbaazmir@icloud.com>
2024-02-28 23:22:04 +0400

    minor changes ~ please ignore

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-require('dotenv').config()
+require("dotenv").config();
 
 // Import necessary modules
 const express = require("express");
-const connection = require('./utils/db');
-const registerRoute = require('./routes/register');
-const courseRoute = require('./routes/course');
-const dotenv = require('dotenv')
+const connection = require("./utils/db");
+const registerRoute = require("./routes/register");
+const courseRoute = require("./routes/course");
+const dotenv = require("dotenv");
 
 // Create an Express application
 const app = express();
@@ -24,13 +24,11 @@ app.use("/course", courseRoute);
 
 // Default route
 app.get("/", (req, res) => {
-    res.send("Hello World");
+  res.send("Hello World");
 });
-
-
 
 // Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
