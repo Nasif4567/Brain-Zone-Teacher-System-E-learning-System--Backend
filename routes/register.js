982d7800e62f73437a28d1a9f865743e84ebb81b
arbaazmir-1 <arbaazmir@icloud.com>
2024-03-12 12:59:37 +0400

    Add webToken utility and integrate with registration process

--- routes/register.js
+++ routes/register.js
@@ -1,33 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const { checkIfExists } = require('../utils/helper');
-const connection = require('../utils/db');
-const {uid} = require('uid');
+const bcrypt = require("bcryptjs");
+const { checkIfExists } = require("../utils/helper");
+const connection = require("../utils/db");
+const { uid } = require("uid");
+const {generateToken} = require("../utils/webToken");
 
 router.post("/", async (req, res) => {
-    const { name, email, password, username } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
+  const { name, email, password, username } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    if(!name || !email || !password || !username) {
-        return res.status(401).send("Please fill all the fields");
+  if (!name || !email || !password || !username) {
+    return res.status(400).send("Please fill all the fields");
+  }
+
+  try {
+    // Check if user already exists from table Users
+    const usernameExists = await checkIfExists("username", username);
+    if (usernameExists) {
+      return res.status(400).send("User already exists");
     }
 
-    try {
-        // Check if user already exists from table Users
-        const usernameExists = await checkIfExists('username', username);
-        if (usernameExists) {
-            return res.status(401).send("User already exists");
-        }
-
-        // Check if email already exists
-        const emailExists = await checkIfExists('email', email);
-        if (emailExists) {
-            return res.status(401).send("Email already exists");
-        }
-        const userID = uid(16);
-        // Insert into table Users
-        /*
+    // Check if email already exists
+    const emailExists = await checkIfExists("email", email);
+    if (emailExists) {
+      return res.status(400).send("Email already exists");
+    }
+    const userID = uid(16);
+    // Insert into table Users
+    /*
         demo data
         {
             "name": "John Doe",
@@ -39,25 +40,33 @@ router.post("/", async (req, res) => {
 
         }
         */
-        connection.query('INSERT INTO teachers SET ?', {
-            teacherID: userID,
-            name: name,
-            email: email,
-            password: hashedPassword,
-            username: username
-        }, (error, results) => {
-            connection.end();
-            if (error) {
-                console.log(error);
-                res.send("Error in registering user");
-            } else {
-                return res.status(200).send("User registered successfully");
-            }
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(401).send("Error in registering user");
-    }
+    connection.query(
+      "INSERT INTO teachers SET ?",
+      {
+        teacherID: userID,
+        name: name,
+        email: email,
+        password: hashedPassword,
+        username: username,
+      },
+      (error, results) => {
+        connection.end();
+        if (error) {
+          console.log(error);
+
+          res.status(400).send("Error in registering user");
+        } else {
+          const token = generateToken({ username, userID });
+          res.cookie("token", token, { httpOnly: true });
+
+          res.status(200).send("User registered successfully");
+        }
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(400).send("Error in registering user");
+  }
 });
 
 module.exports = router;
