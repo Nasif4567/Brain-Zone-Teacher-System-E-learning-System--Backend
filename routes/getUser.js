85e2a84744ece6f2b9aa347499670caa6cd3f15d
arbaazmir-1 <arbaazmir@icloud.com>
2024-03-12 14:58:32 +0400

    Add JWT token verification and error handling

--- routes/getUser.js
+++ routes/getUser.js
@@ -6,6 +6,7 @@ const { uid } = require("uid");
 const { checkIfExists } = require("../utils/helper");
 const { generateToken,verifyToken } = require("../utils/webToken");
 const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 router.use(cookieParser());
 
@@ -13,37 +14,46 @@ router.use(cookieParser());
 router.get("/", async (req, res) => {
     //get data from cookie and verify the token
     const token = req.cookies.token;
-    console.log(token);
-    const payload = verifyToken(token);
-    if (!payload) {
-        return res.status(400).send("Invalid token");
+    if (!token) {
+        return res.status(400).send("Token not provided");
     }
-    const { username } = payload;
-    //get the user from the database
-    connection.query(
-        "SELECT * FROM teachers WHERE username = ?",
-        [username],
-        (error, userResults) => {
-            if (error) {
-                console.error("Error executing SQL query:", error);
-                res.status(400).send("Error in logging user");
-                return;
-            }
 
-            const user = userResults[0];
-            if (!user) {
-                return res.status(400).send("User does not exist");
+    try {
+        const payload = verifyToken(token);
+        console.log(payload);
+        const { username } = payload;
+        
+        //get the user from the database
+        connection.query(
+            "SELECT * FROM teachers WHERE username = ?",
+            [username],
+            (error, userResults) => {
+                if (error) {
+                    console.error("Error executing SQL query:", error);
+                    res.status(400).send("Error in logging user");
+                    return;
+                }
+
+                const user = userResults[0];
+                if (!user) {
+                    return res.status(400).send("User does not exist");
+                }
+                return res.status(200).send({
+                    username: user.username,
+                    email: user.email,
+                    name: user.name
+                });
             }
-            return res.status(200).send({
-                username: user.username,
-                email: user.email,
-                name: user.name
-            });
+        );
+    } catch (err) {
+        if (err instanceof jwt.TokenExpiredError) {
+            return res.status(401).send("Token expired");
+        } else {
+            return res.status(400).send("Invalid token");
         }
-    );
-
-
+    }
 });
+
 
 
 module.exports = router;
