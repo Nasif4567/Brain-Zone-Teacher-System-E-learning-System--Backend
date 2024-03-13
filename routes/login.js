c167fb002bb859362d454b000f26348641cd14f2
arbaazmir-1 <arbaazmir@icloud.com>
2024-03-13 14:18:49 +0400

    Add express-validator for input validation and cleaned the login code

--- routes/login.js
+++ routes/login.js
@@ -2,37 +2,25 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const connection = require("../utils/db");
-const { uid } = require("uid");
-const { checkIfExists } = require("../utils/helper");
 const { generateToken } = require("../utils/webToken");
+const { check, validationResult } = require("express-validator");
 
-router.post("/", async (req, res) => {
-  const {  password, username } = req.body;
+router.post('/', [
+  check('username').trim().escape(),
+  check('password').trim().escape()
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { password, username } = req.body;
 
   try {
-    const usernameExists = await checkIfExists("username", username);
-    
-    if (!usernameExists ) {
-      return res.status(400).send("User does not exist");
-    }
-
-    /* 
-
-    {
-        
-        "email": "johndoe@demo.com",
-        "password": "password",
-        "username": "johndoe"
-
-    }
-
-    */
-
     //get the user from the database
     connection.query(
       "SELECT * FROM teachers WHERE username = ?",
       [username],
-      (error, userResults) => {
+      async (error, userResults) => {
         if (error) {
           console.error("Error executing SQL query:", error);
           res.status(400).send("Error in logging user");
@@ -45,39 +33,27 @@ router.post("/", async (req, res) => {
         }
 
         //check if the password is correct
-        bcrypt.compare(password, user.password, (error, isPasswordCorrect) => {
-          if (error) {
-            console.error("Error comparing passwords:", error);
-            res.status(400).send("Error in logging user");
-            return;
-          }
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) {
+          return res.status(400).send("Password is incorrect");
+        }
+        const tokenExpiresIn = 60 * 60 * 24; // 24 hours
 
-          if (!isPasswordCorrect) {
-            return res.status(400).send("Password is incorrect");
-          }
-          const tokenExpiresIn = 60 * 60 * 24; // 24 hours
+        const token = generateToken({ 
+          username: user.username,
+          teacherID: user.teacherID,
+        }, tokenExpiresIn);
 
-           const token = generateToken({ 
+        res.cookie('token', token, { httpOnly: true, sameSite: 'strict' });
+
+        return res.status(200).send({
+          user:{
             username: user.username,
-            teacherID: user.teacherID,
-            },
-            tokenExpiresIn
-            );
-            res.cookie('token', token, { httpOnly: true, sameSite: 'none'});
-           
-
-            
-          return res.status(200).send(
-            {
-              user:{
-                username: user.username,
-                email: user.email,
-                name: user.name
-              },
-              message : "User logged in successfully",
-              token : token
-            }
-          );
+            email: user.email,
+            name: user.name
+          },
+          message : "User logged in successfully",
+          token : token
         });
       }
     );
